3028c33b5debd44d4a3b95f92d11f4a17047c254
xKachi <kindtheideator@gmail.com>
2024-03-05 13:57:27 +0100

    advanced context usage

---
+++ src/PostContext.js
@@ -0,0 +1,61 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+const PostContext = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    /* The callback function runs on the initial render of the component, and what is returned is stored as the initial state value,
+  In this case, it is a an array of 30 blog post */
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
+  return context;
+}
+
+export { PostProvider, usePosts };
